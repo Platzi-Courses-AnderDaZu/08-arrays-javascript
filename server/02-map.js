ac908cb34a1934e4ec3778f08782063f4a99a6f8
anderDaZu <anderda-96@hotmail.com>
2023-01-24 20:57:37 -0500

    reto para calcular y agregar una nueva propiedad a objetos de un array

--- server/02-map.js
+++ server/02-map.js
@@ -10,3 +10,29 @@ for (let i = 0; i < letters.length; i++) {
 console.log(letters);
 console.log(newArray);
 console.log(newArray2);
+
+// Calcula y agrega una nueva propiedad en un Array
+const array1 = [
+    {
+        name: "Product 1",
+        price: 1000,
+        stock: 10
+    },
+    {
+        name: "Product 2",
+        price: 2000,
+        stock: 20
+    }
+];
+function addNewAttr(array) {
+  const newA = array.map(e => {
+    return {
+        ...e,
+        // taxes: Math.round(e.price * .19) // round aproxima al entero más cercano
+        taxes: Math.trunc(e.price * .19) // trunc solo toma la parte entera
+    }
+  });
+  return newA;
+}
+let result = addNewAttr(array1);
+console.log(result);
